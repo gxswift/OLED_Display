e8aef685aff82eaced94be738512cf243a91f689
gxswift <853129649@qq.com>
2017-08-17 23:27:10 +0800

    驱动文件

---
+++ IIC/myiic.c
@@ -0,0 +1,78 @@
+#include "myiic.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK战舰STM32开发板
+//IIC驱动 代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2012/9/9
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved									  
+//////////////////////////////////////////////////////////////////////////////////
+//初始化IIC
+void IIC_Init(void)
+{					     
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(	RCC_APB2Periph_GPIOB, ENABLE );	
+	   
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP ;   //推挽输出
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOB,GPIO_Pin_10|GPIO_Pin_11); 	//PB10,PB11 输出高
+}
+
+/*********************OLED驱动程序用的延时程序************************************/
+void delay(unsigned int z)
+{
+	unsigned int x,y;
+	for(x=z;x>0;x--)
+		for(y=110;y>0;y--);
+}
+
+/**********************************************
+//IIC Start
+**********************************************/
+void IIC_Start()
+{
+   SCL = high;		
+   SDA = high;
+   SDA = low;
+   SCL = low;
+}
+
+/**********************************************
+//IIC Stop
+**********************************************/
+void IIC_Stop()
+{
+   SCL = low;
+   SDA = low;
+   SCL = high;
+   SDA = high;
+}
+
+/**********************************************
+// 通过I2C总线写一个字节
+**********************************************/
+void Write_IIC_Byte(unsigned char IIC_Byte)
+{
+	unsigned char i;
+	for(i=0;i<8;i++)
+	{
+		if(IIC_Byte & 0x80)
+			SDA=high;
+		else
+			SDA=low;
+		SCL=high;
+		SCL=low;
+		IIC_Byte<<=1;
+	}
+	SDA=1;
+	SCL=1;
+	SCL=0;
+}
+
+
